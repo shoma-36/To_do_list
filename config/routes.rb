7e5f40e0b7b71491e84724834c3b011d14360449
shoma <shoma.ochiai.uec@gmail.com>
2023-06-05 14:22:34 +0000

    complete-to-do-list

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "tasks#index"
   get "/new", to: "tasks#new"
   post "/create", to: "tasks#create"
-  delete "/:id", to: "tasks#destroy"
+  get "/:id", to: "tasks#destroy"
 end
